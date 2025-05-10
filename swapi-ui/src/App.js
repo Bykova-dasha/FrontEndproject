d7add7e557e3f90c13085af15b97d27c09d2a712
Bykova-dasha <dashashultz1305@gmail.com>
2025-05-10 15:56:20 +0200

    added new data

---
+++ swapi-ui/src/App.js
@@ -0,0 +1,55 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="container py-4">
+        <h1 className="text-center mb-4">Star Wars Explorer</h1>
+
+        {/* Фільтри */}
+        <div className="row mb-4">
+          <div className="col-md-4">
+            <input type="text" className="form-control" placeholder="Пошук персонажа..." />
+          </div>
+          <div className="col-md-4">
+            <select className="form-select">
+              <option>Усі категорії</option>
+              <option>Люди</option>
+              <option>Планети</option>
+              <option>Кораблі</option>
+            </select>
+          </div>
+          <div className="col-md-4 text-end">
+            <button className="btn btn-primary">Застосувати фільтри</button>
+          </div>
+        </div>
+
+        {/* Карточки */}
+        <div className="row">
+          <div className="col-md-4 mb-4">
+            <div className="card h-100">
+              <div className="card-body">
+                <h5 className="card-title">Luke Skywalker</h5>
+                <p className="card-text">Height: 172</p>
+                <p className="card-text">Mass: 77</p>
+                <p className="card-text">Gender: Male</p>
+              </div>
+            </div>
+          </div>
+
+          {/* Інші карточки можна дублювати */}
+        </div>
+
+        {/* Пагінація */}
+        <nav className="d-flex justify-content-center mt-4">
+          <ul className="pagination">
+            <li className="page-item disabled"><a className="page-link" href="#">Назад</a></li>
+            <li className="page-item active"><a className="page-link" href="#">1</a></li>
+            <li className="page-item"><a className="page-link" href="#">2</a></li>
+            <li className="page-item"><a className="page-link" href="#">Далі</a></li>
+          </ul>
+        </nav>
+      </div>
+  );
+}
+
+export default App;
